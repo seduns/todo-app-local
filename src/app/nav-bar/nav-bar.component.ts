a6face862e66ac7451622b93edc653de29a8c88c
sedun <rashdanrashid1721@gmail.com>
2025-02-19 22:28:12 +0800

    improve ui, make responsive for mobile view

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+
+  ngOnInit(): void {
+      
+  }
+
+isToggleNav: boolean = false;
+
+toggleNav(): void { 
+  this.isToggleNav = !this.isToggleNav;
+  console.log('Toggle: ' + this.isToggleNav);
+}
+
 
 }
