8f8db61f185664500d82739de0916aa996365478
sedun <rashdanrashid1721@gmail.com>
2025-02-25 01:48:21 +0800

    change side bar for mobile view

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faC, faHome, faListCheck, faTableList } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent implements OnInit {
 
+  faHome = faHome;
+  faTableList = faTableList;
+  faListCheck = faListCheck;
+  isMinimizeNav: boolean = false;
 
   ngOnInit(): void {
       
